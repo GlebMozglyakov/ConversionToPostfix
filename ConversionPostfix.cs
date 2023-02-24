e6429a968893ec5eb5d7c7d2b48cddeb7156849a
GlebMozglyakov <mozglgleb2004@gmail.com>
2023-02-24 19:21:10 +0500

    Добавьте файлы проекта.

---
+++ ConversionPostfix.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionToPostfix
+{
+    public class ConversionPostfix
+    {
+        public static string ConvertToPostfix(string infix)
+        {
+            var postfixSB = new StringBuilder();
+            var operationStack = new Stack<char>();
+            var operationPriority = new Dictionary<char, int>()
+            {
+                {'(', 0 },
+                {'+', 1 },
+                {'-', 1 },
+                {'*', 2 },
+                {'/', 2 },
+                {'^', 3 },
+            };
+
+            if (!string.IsNullOrEmpty(infix))
+            {
+                for (int i = 0; i < infix.Length; i++)
+                {
+                    var symbol = infix[i];
+
+                    if (Char.IsDigit(symbol))
+                        postfixSB.Append(GetNumber(infix, ref i));
+                    else if (symbol == '(')
+                        operationStack.Push(symbol);
+                    else if (symbol == ')')
+                        PopOperationsFromStack(operationStack, ref postfixSB);
+                    else if (operationPriority.ContainsKey(symbol))
+                        AddPriorityOperationsToPostfix(ref operationStack, ref postfixSB, operationPriority, symbol);
+                }
+
+                foreach (var operation in operationStack)
+                    postfixSB.Append(operation);
+            }
+            else
+            {
+                throw new NullReferenceException("Строка не должна быть пустой !");
+            }
+
+            return postfixSB.ToString();
+        }
+
+        private static string GetNumber(string infix, ref int position)
+        {
+            var numberSB = new StringBuilder();
+
+            for (; position < infix.Length; position++)
+            {
+                var number = infix[position];
+
+                if (Char.IsDigit(number))
+                    numberSB.Append(number);
+                else
+                {
+                    position--;
+                    break;
+                }
+            }
+            
+            return numberSB.ToString();
+        }
+
+        private static void PopOperationsFromStack(Stack<char> operationStack, ref StringBuilder postfixSB)
+        {
+            while (operationStack.Count > 0 && operationStack.Peek() != '(')
+                postfixSB.Append(operationStack.Pop());
+            operationStack.Pop();
+        }
+
+        private static void AddPriorityOperationsToPostfix(ref Stack<char> operationStack, ref StringBuilder postfixSB, Dictionary<char, int> operationPriority, char symbol)
+        {
+            while (operationStack.Count > 0 && (operationPriority[operationStack.Peek()] >= operationPriority[symbol]))
+                postfixSB.Append(operationStack.Pop());
+            operationStack.Push(symbol);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConversionToPostfix
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(ConversionPostfix.ConvertToPostfix("(2+1)*3+5"));
+            Console.WriteLine(ConversionPostfix.ConvertToPostfix("3+4"));
+            Console.WriteLine(ConversionPostfix.ConvertToPostfix("(5-4)*2"));
+            Console.WriteLine(ConversionPostfix.ConvertToPostfix("2*(3+4)*5"));
+        }
+    }
+}
